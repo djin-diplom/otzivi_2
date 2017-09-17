04e03c676fb98b63aaa912e67855c6c3520b92fe
21vek <git@github.com>
2017-09-17 11:31:30 +0300

    1

--- requisites.php
+++ requisites.php
@@ -12,3 +12,20 @@ $donor_site_name_4 = 'add@studotzyv.ru';
 //$main_name_corot = "BYPolit.org";
 $site_name = "Вива Диплом";
 $site_name_2 = "add@viva-diplom.ru";
+
+
+$raskrutka_site_1 = 'znaniumbaza.ru';
+$raskrutka_site_2 = 'znaniumbaza';
+$raskrutka_site_3 = 'ZNANIUMBAZA.RU';
+$raskrutka_site_4 = 'znaniumbaza_ru';
+$raskrutka_site_5 = 'ZNANIUMBAZA';
+$raskrutka_site_6 = 'Znaniumbaza';
+
+$raskrutka_site_adress = 'г. Санкт-Петербург, ул. Лермонтова, дом № 8, oф. 184-185';
+
+$raskrutka_site_adress_2 = 'Санкт-Петербург, ул.Лермонтова, д.8';
+
+$raskrutka_site_coord = '59.76351, 30.35746'; //коориднаты на яндексе
+
+$yandex_google = '<meta name="yandex-verification" content="80a43da532801369" /> 
+<meta name="google-site-verification" content="GcsRvOZwbGGTjMazazkuW89BRHJVLRegysoA5XjM_tw" />';
