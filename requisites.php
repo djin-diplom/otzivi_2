0b92f266fe36be3914e72b96e68f1f95ffddb8de
21vek <git@github.com>
2017-10-08 19:26:16 +0300

    1

--- requisites.php
+++ requisites.php
@@ -3,29 +3,28 @@
 date_default_timezone_set('Europe/Moscow');
 $datetime_site = date("Y-m-d H:i:s");
 
-$main_name = "http://vivadip.xyz";//'http://studrabota.com';//"/projects/landingpages/otzivi";
+$main_name = "http://otzivi.16mb.com";
 $donor_name = 'http://studotzyv.ru';
 $donor_site_name = 'Студ Отзыв';
 $donor_site_name_2 = 'Студ-Отзыв';
 $donor_site_name_3 = 'Cтуд-Отзыв';
 $donor_site_name_4 = 'add@studotzyv.ru';
-//$main_name_corot = "BYPolit.org";
-$site_name = "Вива Диплом";
-$site_name_2 = "add@viva-diplom.ru";
+$site_name = "Студенческие отзывы";
+$site_name_2 = "add@otzivi.16mb.com";
 
 
-$raskrutka_site_1 = 'znaniumbaza.ru';
-$raskrutka_site_2 = 'znaniumbaza';
-$raskrutka_site_3 = 'ZNANIUMBAZA.RU';
-$raskrutka_site_4 = 'znaniumbaza_ru';
-$raskrutka_site_5 = 'ZNANIUMBAZA';
-$raskrutka_site_6 = 'Znaniumbaza';
+$raskrutka_site_1 = 'bazaznanij.com';
+$raskrutka_site_2 = 'bazaznanij';
+$raskrutka_site_3 = 'BAZAZNANIJ.COM';
+$raskrutka_site_4 = 'bazaznanij_com';
+$raskrutka_site_5 = 'BAZAZNANIJ';
+$raskrutka_site_6 = 'Bazaznanij';
 
-$raskrutka_site_adress = 'г. Санкт-Петербург, ул. Лермонтова, дом № 8, oф. 184-185';
+$raskrutka_site_adress = 'г. Санкт-Петербург, ул. Ленина, дом № 28, oф. 56';
 
-$raskrutka_site_adress_2 = 'Санкт-Петербург, ул.Лермонтова, д.8';
+$raskrutka_site_adress_2 = 'Санкт-Петербург, ул.Ленина, д.28';
 
-$raskrutka_site_coord = '59.76351, 30.35746'; //коориднаты на яндексе
+$raskrutka_site_coord = '59.962706, 30.301935'; //коориднаты на яндексе
 
-$yandex_google = '<meta name="yandex-verification" content="80a43da532801369" /> 
-<meta name="google-site-verification" content="GcsRvOZwbGGTjMazazkuW89BRHJVLRegysoA5XjM_tw" />';
+$yandex_google = '<meta name="yandex-verification" content="6e4381f481a11919" />
+<meta name="google-site-verification" content="x0bRPcSV-UZ6eX8xQj9xeNn6H0BjKQoxXnU7ZrHTiMM" />';
