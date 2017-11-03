543997905dea33bec8353b1fec6e0f4988a19d39
21vek <git@github.com>
2017-11-03 11:52:08 +0300

    1

--- requisites.php
+++ requisites.php
@@ -3,7 +3,7 @@
 date_default_timezone_set('Europe/Moscow');
 $datetime_site = date("Y-m-d H:i:s");
 
-$main_name = "http://otzivi.16mb.com";
+$main_name = 'http://studotzyv.16mb.com';//"http://otzivi.16mb.com";
 $donor_name = 'http://studotzyv.ru';
 $donor_site_name = 'Студ Отзыв';
 $donor_site_name_2 = 'Студ-Отзыв';
@@ -13,46 +13,20 @@ $site_name = "Студенческие отзывы";
 $site_name_2 = "add@otzivi.16mb.com";
 
 
-$raskrutka_site_1 = 'bazaznanij.com';
-$raskrutka_site_2 = 'bazaznanij';
-$raskrutka_site_3 = 'BAZAZNANIJ.COM';
-$raskrutka_site_4 = 'bazaznanij_com';
-$raskrutka_site_5 = 'BAZAZNANIJ';
-$raskrutka_site_6 = 'Bazaznanij';
+$raskrutka_site_1 = 'bazazachet.ru';
+$raskrutka_site_2 = 'bazazachet';
+$raskrutka_site_3 = 'BAZAZACHET.RU';
+$raskrutka_site_4 = 'bazazachet_ru';
+$raskrutka_site_5 = 'BAZAZACHET';
+$raskrutka_site_6 = 'bazazachet';
 
-$raskrutka_site_adress = 'г. Санкт-Петербург, ул. Ленина, дом № 28, oф. 56';
+$raskrutka_site_adress = 'г. Санкт-Петербург, Каменноостровский пр-кт, дом № 27, oф. 144-145';
 
-$raskrutka_site_adress_2 = 'Санкт-Петербург, ул.Ленина, д.28';
+$raskrutka_site_adress_2 = 'Санкт-Петербург, Каменноостровский пр-кт, д.27';
 
-$raskrutka_site_coord = '59.962706, 30.301935'; //коориднаты на яндексе
+$raskrutka_site_coord = '59.964687, 30.314664'; //коориднаты на яндексе
 
 $yandex_google = '<meta name="yandex-verification" content="6e4381f481a11919" />
 <meta name="google-site-verification" content="x0bRPcSV-UZ6eX8xQj9xeNn6H0BjKQoxXnU7ZrHTiMM" />
-<!-- Yandex.Metrika counter -->
-<script type="text/javascript" >
-    (function (d, w, c) {
-        (w[c] = w[c] || []).push(function() {
-            try {
-                w.yaCounter46355988 = new Ya.Metrika({
-                    id:46355988,
-                    clickmap:true,
-                    trackLinks:true,
-                    accurateTrackBounce:true
-                });
-            } catch(e) { }
-        });
 
-        var n = d.getElementsByTagName("script")[0],
-            s = d.createElement("script"),
-            f = function () { n.parentNode.insertBefore(s, n); };
-        s.type = "text/javascript";
-        s.async = true;
-        s.src = "https://mc.yandex.ru/metrika/watch.js";
-
-        if (w.opera == "[object Opera]") {
-            d.addEventListener("DOMContentLoaded", f, false);
-        } else { f(); }
-    })(document, window, "yandex_metrika_callbacks");
-</script>
-<noscript><div><img src="https://mc.yandex.ru/watch/46355988" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
-<!-- /Yandex.Metrika counter -->';
+';
