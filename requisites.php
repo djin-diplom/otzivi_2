06bc3b2e9bc5fed70179eb7a16bb74615d98a2e4
21vek <git@github.com>
2017-11-29 12:07:31 +0300

    1

--- requisites.php
+++ requisites.php
@@ -3,28 +3,28 @@
 date_default_timezone_set('Europe/Moscow');
 $datetime_site = date("Y-m-d H:i:s");
 
-$main_name = 'http://studotzyv.16mb.com';//"http://otzivi.16mb.com";
+$main_name = 'http://otzyv.96.lt';
 $donor_name = 'http://studotzyv.ru';
 $donor_site_name = 'Студ Отзыв';
 $donor_site_name_2 = 'Студ-Отзыв';
 $donor_site_name_3 = 'Cтуд-Отзыв';
 $donor_site_name_4 = 'add@studotzyv.ru';
 $site_name = "Студенческие отзывы";
-$site_name_2 = "add@otzivi.16mb.com";
+$site_name_2 = "add@otzyv.96.lt";
 
 
-$raskrutka_site_1 = 'bazazachet.ru';
-$raskrutka_site_2 = 'bazazachet';
-$raskrutka_site_3 = 'BAZAZACHET.RU';
-$raskrutka_site_4 = 'bazazachet_ru';
-$raskrutka_site_5 = 'BAZAZACHET';
-$raskrutka_site_6 = 'bazazachet';
+$raskrutka_site_1 = 'basisznanij.ru';
+$raskrutka_site_2 = 'basisznanij';
+$raskrutka_site_3 = 'BASISZNANIJ.RU';
+$raskrutka_site_4 = 'basisznanij_ru';
+$raskrutka_site_5 = 'BASISZNANIJ';
+$raskrutka_site_6 = 'basisznanij';
 
-$raskrutka_site_adress = 'г. Санкт-Петербург, Каменноостровский пр-кт, дом № 27, oф. 144-145';
+$raskrutka_site_adress = 'г. Санкт-Петербург, ул. Пушкина, дом № 15, oф. 104-105';
 
-$raskrutka_site_adress_2 = 'Санкт-Петербург, Каменноостровский пр-кт, д.27';
+$raskrutka_site_adress_2 = 'Санкт-Петербург, ул. Пушкина, д.15';
 
-$raskrutka_site_coord = '59.964687, 30.314664'; //коориднаты на яндексе
+$raskrutka_site_coord = '60.188653, 29.806658'; //коориднаты на яндексе
 
 $yandex_google = '<meta name="yandex-verification" content="6e4381f481a11919" />
 <meta name="google-site-verification" content="x0bRPcSV-UZ6eX8xQj9xeNn6H0BjKQoxXnU7ZrHTiMM" />
