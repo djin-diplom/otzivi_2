530500f2f6cdd83f275dda97c35370698da9836c
21vek <git@github.com>
2018-01-04 12:11:14 +0300

    1

--- requisites.php
+++ requisites.php
@@ -3,30 +3,29 @@
 date_default_timezone_set('Europe/Moscow');
 $datetime_site = date("Y-m-d H:i:s");
 
-$main_name = 'http://otzyv.96.lt';
+$main_name = 'http://otzyv.890m.com';
 $donor_name = 'http://studotzyv.ru';
 $donor_site_name = 'Студ Отзыв';
 $donor_site_name_2 = 'Студ-Отзыв';
 $donor_site_name_3 = 'Cтуд-Отзыв';
 $donor_site_name_4 = 'add@studotzyv.ru';
 $site_name = "Студенческие отзывы";
-$site_name_2 = "add@otzyv.96.lt";
+$site_name_2 = "add@otzyv.890m.com";
 
 
-$raskrutka_site_1 = 'basisznanij.ru';
-$raskrutka_site_2 = 'basisznanij';
-$raskrutka_site_3 = 'BASISZNANIJ.RU';
-$raskrutka_site_4 = 'basisznanij_ru';
-$raskrutka_site_5 = 'BASISZNANIJ';
-$raskrutka_site_6 = 'basisznanij';
+$raskrutka_site_1 = 'vivazachet.ru';
+$raskrutka_site_2 = 'vivazachet';
+$raskrutka_site_3 = 'VIVAZACHET.RU';
+$raskrutka_site_4 = 'vivazachet_ru';
+$raskrutka_site_5 = 'VIVAZACHET';
+$raskrutka_site_6 = 'vivazachet';
 
-$raskrutka_site_adress = 'г. Санкт-Петербург, ул. Пушкина, дом № 15, oф. 104-105';
+$raskrutka_site_adress = 'г. Санкт-Петербург, проспект Энергетиков, дом № 26, oф. 98-99';
 
-$raskrutka_site_adress_2 = 'Санкт-Петербург, ул. Пушкина, д.15';
+$raskrutka_site_adress_2 = 'Санкт-Петербург, проспект Энергетиков, д.26';
 
-$raskrutka_site_coord = '60.188653, 29.806658'; //коориднаты на яндексе
+$raskrutka_site_coord = '59.954570, 30.436215'; //коориднаты на яндексе
 
 $yandex_google = '<meta name="yandex-verification" content="6e4381f481a11919" />
-<meta name="google-site-verification" content="x0bRPcSV-UZ6eX8xQj9xeNn6H0BjKQoxXnU7ZrHTiMM" />
-
+<meta name="google-site-verification" content="tshUkmsPhSUMJyHn8BAY4QCqYRiTqexr95J2IeUXDuI" />
 ';
