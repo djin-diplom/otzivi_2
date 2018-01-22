3f33c17e2ee69b0c8c0721e6d2248586a7f6d8e5
21vek <git@github.com>
2018-01-22 17:45:21 +0300

    1

--- requisites.php
+++ requisites.php
@@ -3,7 +3,7 @@
 date_default_timezone_set('Europe/Moscow');
 $datetime_site = date("Y-m-d H:i:s");
 
-$main_name = 'http://http://otzyv.hol.es';
+$main_name = 'http://otzyv.hol.es';
 $donor_name = 'http://studotzyv.ru';
 $donor_site_name = 'Студ Отзыв';
 $donor_site_name_2 = 'Студ-Отзыв';
