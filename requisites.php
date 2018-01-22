827357444c6f8d146ff297d84c773f167d256168
21vek <git@github.com>
2018-01-22 17:29:01 +0300

    1

--- requisites.php
+++ requisites.php
@@ -3,28 +3,28 @@
 date_default_timezone_set('Europe/Moscow');
 $datetime_site = date("Y-m-d H:i:s");
 
-$main_name = 'http://otzyv.890m.com';
+$main_name = 'http://http://otzyv.hol.es';
 $donor_name = 'http://studotzyv.ru';
 $donor_site_name = 'Студ Отзыв';
 $donor_site_name_2 = 'Студ-Отзыв';
 $donor_site_name_3 = 'Cтуд-Отзыв';
 $donor_site_name_4 = 'add@studotzyv.ru';
 $site_name = "Студенческие отзывы";
-$site_name_2 = "add@otzyv.890m.com";
+$site_name_2 = "add@otzyv.hol.es";
 
 
-$raskrutka_site_1 = 'vivazachet.ru';
-$raskrutka_site_2 = 'vivazachet';
-$raskrutka_site_3 = 'VIVAZACHET.RU';
-$raskrutka_site_4 = 'vivazachet_ru';
-$raskrutka_site_5 = 'VIVAZACHET';
-$raskrutka_site_6 = 'vivazachet';
+$raskrutka_site_1 = 'vivastudent.ru';
+$raskrutka_site_2 = 'vivastudent';
+$raskrutka_site_3 = 'VIVASTUDENT.RU';
+$raskrutka_site_4 = 'vivastudent_ru';
+$raskrutka_site_5 = 'VIVASTUDENT';
+$raskrutka_site_6 = 'vivastudent';
 
-$raskrutka_site_adress = 'г. Санкт-Петербург, проспект Энергетиков, дом № 26, oф. 98-99';
+$raskrutka_site_adress = 'г. Санкт-Петербург, ул. Льва Толстого, дом № 19, oф. 45-46';
 
-$raskrutka_site_adress_2 = 'Санкт-Петербург, проспект Энергетиков, д.26';
+$raskrutka_site_adress_2 = 'Санкт-Петербург, ул. Льва Толстого, д.19';
 
-$raskrutka_site_coord = '59.954570, 30.436215'; //коориднаты на яндексе
+$raskrutka_site_coord = '59.964273, 30.321348'; //коориднаты на яндексе
 
 $yandex_google = '<meta name="yandex-verification" content="da198a1694ea46a9" />
 <meta name="google-site-verification" content="tshUkmsPhSUMJyHn8BAY4QCqYRiTqexr95J2IeUXDuI" />
